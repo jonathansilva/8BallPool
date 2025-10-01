f64b259802b66462bcfe8ecadba0bc32f2108469
Huy Nguyen <huythedev@gmail.com>
2025-10-01 22:12:18 +0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/app/hack/eightballpool/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/app/hack/eightballpool/ExampleInstrumentedTest.java
@@ -2,7 +2,7 @@ package app.hack.eightballpool;
 
 import android.content.Context;
 
-import androidx.test.platform.app   .InstrumentationRegistry;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
